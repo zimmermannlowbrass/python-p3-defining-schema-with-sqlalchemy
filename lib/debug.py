176aec5dc0b405fdabe8247a526d3ea62cac3921
Ben Botsford <ben.botsford@flatironschool.com>
2022-10-26 09:51:47 -0500

    remove ref to db folder

--- lib/debug.py
+++ lib/debug.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 
 from sqlalchemy_sandbox import Student
 
-engine = create_engine('sqlite:///db/students.db')
+engine = create_engine('sqlite:///students.db')
 
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
